365c53034dba048a5b10cd64ba2fc08b1be9d338
Austin Quach <austinquachh@gmail.com>
2018-04-20 00:37:07 -0700

    initial push

---
+++ app/src/main/java/austinqq/washington/edu/activityspy/MainActivity.kt
@@ -0,0 +1,21 @@
+package austinqq.washington.edu.activityspy
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.i(TAG, "onCreate event fired" + intent)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e(TAG, "We're going down, Captain!" + intent)
+    }
+}
